20d88c63a7c9bcf7cfb9ef54de51138390f695c5
Marcos <marcos.ruiz@enti.cat>
2023-07-02 17:46:25 +0200

    KeyObjects

---
+++ Assets/Scripts/MovingItems/KeyItems.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New KeyObject", menuName = "KeyObject")]
+public class KeyItems : ScriptableObject
+{ 
+  
+    private enum keyItem { Axe, Ladder, ScrewDriver, Hammer};
+
+    [SerializeField]
+    private keyItem items;
+
+    public string GetItem()
+    {
+        return items.ToString();
+    }
+}
