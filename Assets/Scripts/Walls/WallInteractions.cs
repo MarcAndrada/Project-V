56be116ccfddc755f52aae9d2dab9d479dd600e4
Marcos <marcos.ruiz@enti.cat>
2023-07-02 20:44:12 +0200

    Wall interactions

--- Assets/Scripts/Walls/WallInteractions.cs
+++ Assets/Scripts/Walls/WallInteractions.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WallInteractions : MonoBehaviour
 {
@@ -10,15 +11,45 @@ public class WallInteractions : MonoBehaviour
     [SerializeField]
     Keyitem item;
 
+    [SerializeField]
+    private GameObject allow;
+
+    [SerializeField]
+    private GameObject prohibited;
+
+    [SerializeField]
+    private bool canBreak;
     private void OnTriggerEnter(Collider other)
     {
         if (keyItems == item.GetItem() && other.CompareTag("KeyItem")) //Faltaría pasarle que el player esta sujetando un objeto, porque si no simplemene podrá interecturar aun estando el objeto en el suelo
         {
-            Debug.Log("SI");
+            canBreak = true;
+            allow.SetActive(true);
+            prohibited.SetActive(false);
         }
         else
         {
-            Debug.Log("NO");
+            prohibited.SetActive(true);
         }
     }
+    private void OnTriggerExit(Collider other) 
+    {
+        if (keyItems == item.GetItem() && other.CompareTag("KeyItem"))
+        { 
+            canBreak = false;
+            allow.SetActive(false);
+            prohibited.SetActive(false);
+        }
+    }
+    private void Update()
+    {
+        if(canBreak && Input.GetKeyDown(KeyCode.Q))
+        {
+            allow.SetActive(false);
+            prohibited.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+
+
 }
