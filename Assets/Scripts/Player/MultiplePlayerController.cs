900c64182afce909972c7ece7552aae4b5a9a40d
Cramitus <73467859+MarcAndrada@users.noreply.github.com>
2023-07-07 15:35:44 +0200

    Tengo un gran problema, soy adicto a las tetas 😔

--- Assets/Scripts/Player/MultiplePlayerController.cs
+++ Assets/Scripts/Player/MultiplePlayerController.cs
@@ -1,20 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MultiplePlayerController : MonoBehaviour
 {
     private CameraController cameraController;
-
+    private PlayersReadyController playerReadyController;
     private void Awake()
     {
         cameraController = FindObjectOfType<CameraController>();
+        playerReadyController = FindObjectOfType<PlayersReadyController>(); 
     }
 
     public void JoinnedPlayer(PlayerInput obj)
     {
         cameraController.AddPlayer(obj.gameObject);
+        playerReadyController.AddPlayer(obj);
     }
     public void LeftPlayer(PlayerInput obj)
     {
